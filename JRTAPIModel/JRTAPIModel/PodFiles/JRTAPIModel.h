7be78fb27896f88cfa79ae5c4f1eb3c455269f41
Juan Garcia <juancarlos.garcia.alfaro@gmail.com>
2016-01-20 11:31:43 -0600

    import

--- JRTAPIModel/JRTAPIModel/PodFiles/JRTAPIModel.h
+++ JRTAPIModel/JRTAPIModel/PodFiles/JRTAPIModel.h
@@ -21,7 +21,7 @@
 //This is an Alpha Version, Updating for AFNetworking 3.0.4
 
 #import <JSONModel/JSONModel.h>
-#import <AFHTTPSessionManager.h>
+#import <AFNetworking/AFNetworking.h>
 
 #pragma mark - Constants
 extern NSString * const kAPI_URL;
